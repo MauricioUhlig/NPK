e81f06e60fee67ea3694fea1c10cf99c707b5dfb
MauricioUhlig <mauriciojastrowuhlig@gmail.com>
2016-12-16 00:17:36 -0200

    Commit

---
+++ app/src/main/java/br/com/softweb/npk/activity/submenu/fragaduba/FragmentAZ.java
@@ -0,0 +1,31 @@
+package br.com.softweb.npk.activity.submenu.fragaduba;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.softweb.npk.R;
+
+
+public class FragmentAZ extends Fragment{
+
+    public FragmentAZ() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alfabetico, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/br/com/softweb/npk/activity/submenu/fragaduba/FragmentGrupo.java
@@ -0,0 +1,30 @@
+package br.com.softweb.npk.activity.submenu.fragaduba;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.softweb.npk.R;
+
+
+public class FragmentGrupo extends Fragment{
+
+    public FragmentGrupo() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_grupo, container, false);
+    }
+
+}
